4d753493057c1b051f95d842d0e20bfdd2b44d2a
Gyoungmin Park <gyopark@c2r1s3.42seoul.kr>
2022-12-13 18:52:29 +0900

    12/13

---
+++ get_next_line/main.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gyopark <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/02 19:13:22 by gyopark           #+#    #+#             */
+/*   Updated: 2022/12/12 18:17:51 by gyopark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <limits.h>
+#include <sys/fcntl.h>
+
+int	main(void)
+{
+	char	*line;
+	int		fd;
+	int		i;
+
+	i = 1;
+	fd = open("test.txt", O_RDONLY);
+	printf("fd : %d\n", fd);
+	while (fd)
+	{
+		line = get_next_line(fd);
+		if (!line)
+			break ;
+		printf("Gnl %d line : %s", i++, line);
+		free(line);
+	}
+	free(line);
+	close(fd);
+	return (0);
+}
